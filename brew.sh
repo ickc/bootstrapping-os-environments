5ce82c7d4c8f4e7346aea310fae16f33eb7e82df
KolenCheung <christian.kolen@gmail.com>
2016-09-20 18:24:00 -0700

    initial commit

---
+++ brew.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# brew cask
+
+# license required
+brew cask install microsoft-office
+brew cask install adobe-creative-cloud
+brew cask install vmware-fusion
+brew cask install atext
+brew cask install textmate
+brew cask install papers
+
+# services
+brew cask install dropbox
+brew cask install google-drive
+brew cask install github-desktop
+brew cask install duet
+# brew cask install steam
+# brew cask install manuscripts
+# brew cask install marked
+
+# free
+brew cask install mactex
+brew cask install atom
+# brew cask install macdown
+
+# browser
+brew cask install google-chrome
+brew cask install firefox
+
+# media
+brew cask install keka
+brew cask install inkscape
+brew cask install adobe-dng-converter
+brew cask install imageoptim
+brew cask install imagealpha
+brew cask install handbrake
+brew cask install vlc
+brew cask install makemkv
+brew cask install kodi
+# brew cask install mplayerx
+
+# small utilities
+brew cask install desktoputility
+brew cask install disk-inventory-x
+brew cask install dupeguru
+brew cask install filezilla
+brew cask install sdformatter
+brew cask install appcleaner
+brew cask install namechanger
+# brew cask install calibre
+# brew cask install google-earth
+# brew cask install stellarium
+# brew cask install nimble
+# brew cask install blue-jeans-launcher
+
+# hardware related
+brew cask install logitech-options
+brew cask install gfxcardstatus
+brew cask install logitech-gaming-software
+brew cask install cuda-z
+brew cask install cuda
+brew cask install intel-power-gadget
+brew cask install wacom-intuos-tablet # check if it is this or `brew cask install wacom-intuos-pro-tablet`
+# brew cask install paragon-ntfs # Seagate version??
+
+# runtime
+brew cask install flash-player
+brew cask install java
+# brew cask install silverlight
+
+# using npm
+brew install node
+npm install -g imageoptim-cli
+
+# No cli install:
+
+# Safari Preview
+# Chrome Canary
+# switchresx
+# Nvidia Driver Manager
+# iOS Font Maker
+# Intuos
+# ColorMunki Photo
+# Logos
+# SMARTReporter
+# CHM Reader
+# Cisco AnyConnect
+# Sophos Antivirus
+# Davinci Resolve
+# mathematica
+# tsmuxerGUI
+# Brother ControlCenter
+# ti connect
+# Remoate Camera Control
+# MurGaa Auto Clicker
+# MultiMarkdown Composer
+# Window Tidy
---
+++ list-installed-packages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd "$@"
+
+# Mac App Stores Only
+mas list > mas.txt
+
+# Applications (including Mac App Stores')
+ls /Applications > applicatio-ls.txt
+tree -L 2 /Applications > applications-tree.txt
+
+# Safari extensions
+find ~/Library/Safari/Extensions/ -iname '*.safariextz' -print | sed -e s=/.*/==g -e s=.safariextz==g > safari.txt
+
+# local bin in home
+ls ~/.local/bin > home-local-bin-ls.txt
+tree ~/.local/bin > home-local-bin-tree.txt
+
+# brew
+brew leaves > brew.txt
+
+# gem
+gem list --no-version > gem.txt
+
+#npm
+npm list --depth=0 > npm-project-tree.txt
+npm list -g --depth=0 > npm-global-tree.txt
+ls `npm root -g` > npm-global-ls.txt
+
+# pip
+pip list | sed 's/ (.*)//' > pip.txt
