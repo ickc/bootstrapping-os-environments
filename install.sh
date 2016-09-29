102109ede94c866c377f8e0214e672fa455e887e
ickc <ickc@users.noreply.github.com>
2016-09-29 09:19:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install xcode
 xcode-select --install
-sudo xcodebuild -license
+sudo xcodebuild -license accept
 
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
