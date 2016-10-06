92dcaa3994b3aa19e954c7d5cad1eb1d44c39236
KolenCheung <christian.kolen@gmail.com>
2016-10-06 16:41:39 -0700

    add minify tools

--- npm.sh
+++ npm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 npm install -g imageoptim-cli
-npm install -g uglify-js
 npm install -g mathjax-node
+npm install -g uglify-js
+npm install -g cssnano-cli
+npm install -g csso
+npm install -g html-minifier
