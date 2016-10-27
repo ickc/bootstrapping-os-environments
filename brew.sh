e92628d755402bb9223f8411c9b6a1579fd0e490
Kolen Cheung <christian.kolen@gmail.com>
2016-10-27 11:30:42 -0700

    use cabal to install pandoc instead

--- brew.sh
+++ brew.sh
@@ -5,8 +5,6 @@ brew install python3 && brew linkapps python3
 brew install ruby && echo 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> ~/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
 # brew install rbenv && rbenv init
 brew install multimarkdown
-brew install pandoc
-brew install pandoc-citeproc
 brew install openconnect # Open client for Cisco AnyConnect VPN
 brew install enca
 brew install smartmontools
--- cabal.sh
+++ cabal.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cabal install --allow-newer=base pandoc-csv2table
+cabal install --force-reinstalls pandoc
+cabal install --force-reinstalls pandoc-citeproc
+cabal install --force-reinstalls --allow-newer=base pandoc-csv2table
+cabal install --force-reinstalls -f inlineMarkdown pandoc-placetable
