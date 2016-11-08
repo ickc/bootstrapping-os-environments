ce8120944d28bbb4f8868090feb27aa72d6dd417
Kolen Cheung <christian.kolen@gmail.com>
2016-11-08 15:06:45 -0800

    pip: setup jupyter

--- pip.sh
+++ pip.sh
@@ -9,4 +9,4 @@ pip install -U nose
 pip install -U pandas
 pip install -U sympy
 pip install -U cython
-pip install -U jupyter
+pip install -U jupyter && printf "%s\n" "c.NotebookApp.browser = '/Applications/Firefox.app/Contents/MacOS/firefox-bin %s'" > $HOME/.jupyter/jupyter_notebook_config.py
