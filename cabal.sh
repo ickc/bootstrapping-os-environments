c68974c183acf4dc30e5048c00b57008db5782b7
Kolen Cheung <christian.kolen@gmail.com>
2016-12-10 17:56:59 -0800

    remove cabal.sh

--- cabal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cabal install --force-reinstalls pandoc
-cabal install --force-reinstalls pandoc-citeproc
-cabal install --force-reinstalls --allow-newer=base pandoc-csv2table
-cabal install --force-reinstalls -f inlineMarkdown pandoc-placetable
