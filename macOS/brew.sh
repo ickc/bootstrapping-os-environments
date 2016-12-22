e56f0a0b70865ebd446e04dfb01cdc0d8d4bda22
Kolen Cheung <christian.kolen@gmail.com>
2016-12-22 00:22:39 -0800

    brew.sh: alias python to python3

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -4,3 +4,5 @@ grep -v '#' brew.txt | xargs brew install
 
 brew linkapps python3
 printf "%s\n" "" '# homebrew's Ruby'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
+
+printf "%s\n" "" '# default python3' 'alias python=python3' >> $HOME/.bash_profile
