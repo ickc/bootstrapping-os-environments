1585b4ca1eb557340bb61fcf14ebf9dac2d53826
Kolen Cheung <christian.kolen@gmail.com>
2017-02-14 16:40:24 -0800

    pip.sh: install jupyter extension

--- common/pip.sh
+++ common/pip.sh
@@ -3,3 +3,6 @@
 pip3 install -r pip.txt
 
 printf "%s\n" "c.NotebookApp.browser = '/Applications/Firefox.app/Contents/MacOS/firefox-bin %s'" > $HOME/.jupyter/jupyter_notebook_config.py
+
+# install jupyter widget extension
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
