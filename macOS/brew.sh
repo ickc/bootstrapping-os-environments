9d6ed13c2748b5a7e2ebd603cc51cf83415ff0c9
Kolen Cheung <christian.kolen@gmail.com>
2017-02-15 18:22:21 -0800

    brew: gcc add flag

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -2,6 +2,9 @@
 
 grep -v '#' brew.txt | xargs brew install
 
+# multilib is for compiling for other architechture, which may not work with OpenMP
+brew reinstall gcc --without-multilib
+
 brew linkapps python3
 printf "%s\n" "" '# homebrew's Ruby'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
 
