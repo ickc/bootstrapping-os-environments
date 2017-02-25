0ea3e2b9933f540c58b495a41f5b6279b1a37ea0
Kolen Cheung <christian.kolen@gmail.com>
2017-02-24 21:20:32 -0800

    brew add sshfs

--- macOS/brew-cask.sh
+++ macOS/brew-cask.sh
@@ -7,6 +7,8 @@ brew tap caskroom/versions
 
 # grep -v invert the search. i.e. all lines including # are considered as "comments"
 grep -v '#' brew-cask.txt | xargs brew cask install
+# install this after brew cask install osxfuse
+brew install homebrew/fuse/sshfs
 
 # overriding Mac App Store's version
 brew cask install --force atext multimarkdown-composer-beta
