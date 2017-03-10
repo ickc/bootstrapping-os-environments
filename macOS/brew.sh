6502ba246d5fc85126d242305f0d22fb86bb7476
Kolen Cheung <christian.kolen@gmail.com>
2017-03-10 11:37:27 -0800

    brew: refactor mpich; migrate gcc to gnuize

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# only when build from source, it will uses homebrew's compiler (chosen as gcc from gnuize)
+brew install mpich --build-from-source
+
 grep -v '#' brew.txt | xargs brew install
 
-# multilib is for compiling for other architechture, which may not work with OpenMP
-brew reinstall gcc --without-multilib
-
+# link the apps to /Applications
 brew linkapps python3
+
 printf "%s\n" "" '# homebrew's Ruby'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
 
 printf "%s\n" "" '# default python3' 'alias python=python3' >> $HOME/.bash_profile
