fd627d4866389e326eeff175c9165c78896f7be9
Kolen Cheung <christian.kolen@gmail.com>
2017-03-11 14:59:46 -0800

    deb: add dropbox

--- ubuntu/deb.sh
+++ ubuntu/deb.sh
@@ -3,4 +3,8 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Gitkraken
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && sudo dpkg -i gitkraken-amd64.deb
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && \
+	sudo dpkg -i gitkraken-amd64.deb
+# dropbox
+wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb && \
+	sudo dpkg -i dropbox_2015.10.28_amd64.deb
