47dcbc563bd73272839b746b541bb20291419399
Kolen Cheung <christian.kolen@gmail.com>
2017-03-31 19:33:05 -0700

    pip.sh: add U flag

--- common/pip.sh
+++ common/pip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install -r pip.txt
+pip3 install -Ur pip.txt
 
 printf "%s\n" "c.NotebookApp.browser = '/Applications/Firefox.app/Contents/MacOS/firefox-bin %s'" > $HOME/.jupyter/jupyter_notebook_config.py
 
