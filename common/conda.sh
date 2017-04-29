3dd7de5799bcaa0e4c6bd6cd52f1c2359f23d03c
Kolen Cheung <christian.kolen@gmail.com>
2017-04-28 20:17:22 -0700

    conda.sh: add Intel's to Jupyter kernel

--- common/conda.sh
+++ common/conda.sh
@@ -6,3 +6,7 @@ grep -v '#' conda.txt | xargs -n 1 -P 1 conda install
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
 conda create -n idp3 python=3 ipykernel
+
+. activate idp3
+
+python -m ipykernel install --user --name idp3 --display-name "Python 3 (Intel)"
