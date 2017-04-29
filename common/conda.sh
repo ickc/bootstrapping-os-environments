ce0363fe5a08b0a08cf066c3142046a22e8c1b7e
Kolen Cheung <christian.kolen@gmail.com>
2017-04-28 19:44:14 -0700

    conda.sh: create idp3 env
    stand for Intel Distribution for Python 3

--- common/conda.sh
+++ common/conda.sh
@@ -4,3 +4,5 @@ grep -v '#' conda.txt | xargs -n 1 -P 1 conda install
 
 # install jupyter widget extension
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
+conda create -n idp3 python=3 ipykernel
