216484313197c0eea027be14db8b6faed9945220
Kolen Cheung <christian.kolen@gmail.com>
2017-06-28 13:02:44 -0700

    brew*.*: sshfs migrated to brew

--- macOS/brew-cask.sh
+++ macOS/brew-cask.sh
@@ -8,8 +8,6 @@ brew tap caskroom/drivers
 
 # grep -v invert the search. i.e. all lines including # are considered as "comments"
 grep -v '#' brew-cask.txt | xargs brew cask install
-# install this after brew cask install osxfuse
-brew install homebrew/fuse/sshfs
 
 # overriding Mac App Store's version
 brew cask install --force atext multimarkdown-composer-beta
