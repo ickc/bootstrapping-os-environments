5cf15cc9aa7f6daebff97953e8b3826e98b2378d
Kolen Cheung <christian.kolen@gmail.com>
2017-06-28 11:22:58 -0700

    brew.sh: fix syntax error

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -8,6 +8,6 @@ grep -v '#' brew.txt | xargs brew install
 # link the apps to /Applications
 brew linkapps python3
 
-printf "%s\n" "" '# homebrew's Ruby'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
+printf "%s\n" "" '# Ruby from homebrew'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
 
 printf "%s\n" "" '# default python3' 'alias python=python3' >> $HOME/.bash_profile
