5a8cb94af0ecb0a6b50628a05fafea0c71ce1e92
Kolen Cheung <christian.kolen@gmail.com>
2017-06-30 16:32:00 -0700

    pip.sh: fix possibly missing dir

--- common/pip.sh
+++ common/pip.sh
@@ -2,6 +2,7 @@
 
 pip3 install -Ur pip.txt
 
+mkdir -p $HOME/.jupyter/
 printf "%s\n" "c.NotebookApp.browser = '/Applications/Firefox.app/Contents/MacOS/firefox-bin %s'" > $HOME/.jupyter/jupyter_notebook_config.py
 
 # install jupyter widget extension
