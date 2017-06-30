687f92ac7e11f58c589f9e4893bb65d22dd38ede
Kolen Cheung <christian.kolen@gmail.com>
2017-06-30 16:31:38 -0700

    conda.sh: install packages with the idp3 env

--- common/conda.sh
+++ common/conda.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-grep -v '#' conda.txt | xargs -n 1 -P 1 conda install
-
-# install jupyter widget extension
-jupyter nbextension enable --py --sys-prefix widgetsnbextension
-
 conda create -n idp3 python=3 ipykernel
 
 . activate idp3
 
-python -m ipykernel install --user --name idp3 --display-name "Python 3 (Intel)"
+python -m ipykernel install --user --name idp3 --display-name "IDP3"
+
+grep -v '#' conda.txt | xargs -n 1 -P 1 conda install
+
+# install jupyter widget extension
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
