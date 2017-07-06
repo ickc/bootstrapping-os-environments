63611b35f65f78cd1545307267f59db7e9a0f430
Kolen Cheung <christian.kolen@gmail.com>
2017-07-06 15:36:22 -0700

    pip.sh: use pip instead of pip3

--- common/pip.sh
+++ common/pip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install -Ur pip.txt
+pip install -Ur pip.txt
 
 mkdir -p $HOME/.jupyter/
 printf "%s\n" "c.NotebookApp.browser = '/Applications/Firefox.app/Contents/MacOS/firefox-bin %s'" > $HOME/.jupyter/jupyter_notebook_config.py
