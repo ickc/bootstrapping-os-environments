07707fe16335e5ae2cd0fc59dd3645aaad070ad2
Kolen Cheung <christian.kolen@gmail.com>
2017-07-17 14:36:22 -0700

    conda.*: refactor ipykernel; force using intel channel

--- common/conda.sh
+++ common/conda.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-conda create -n idp3 python=3 ipykernel
+conda create -n idp3 -c intel intelpython2_core python=3
 
 . activate idp3
-
-python -m ipykernel install --user --name idp3 --display-name "IDP3"
 
 grep -v '#' conda.txt | xargs -n 1 -P 1 conda install -y
 # install the mpi4py with mpich
 conda install -c mpi4py mpi4py mpich -y
+
+# iPython kernel
+python -m ipykernel install --user --name idp3 --display-name "IDP3"
 
 # install jupyter widget extension
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
