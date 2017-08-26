62a730d583714eff6fcc6bdcdd97e7a235a0d8d6
Kolen Cheung <christian.kolen@gmail.com>
2017-08-26 16:40:31 -0700

    brew-cask.sh: anaconda use activate instead of PATH

--- macOS/brew-cask.sh
+++ macOS/brew-cask.sh
@@ -15,4 +15,5 @@ brew cask install --force atext multimarkdown-composer-beta
 # CUDA PATH
 printf "%s\n" "" "# CUDA" 'export PATH="'$(echo /Developer/NVIDIA/CUDA-*.*/bin)':$PATH"' >> $HOME/.bash_profile
 # conda PATH
-printf "%s\n" "" "# conda" 'export PATH=/usr/local/anaconda3/bin:"$PATH"' >> $HOME/.bash_profile
+# this is a better approach since you can always deactivate it and uses defaults bin for example
+printf "%s\n" "" "# conda" '. /usr/local/anaconda3/bin/activate root' >> $HOME/.bash_profile
