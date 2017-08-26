940decd0526360112701625d5cc661c80ec509ae
Kolen Cheung <christian.kolen@gmail.com>
2017-08-26 00:12:18 -0700

    brew.*: remove python (favor anaconda)

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -5,9 +5,4 @@ brew install mpich --build-from-source
 
 grep -v '#' brew.txt | xargs brew install
 
-# link the apps to /Applications
-brew linkapps python3
-
 printf "%s\n" "" '# Ruby from homebrew'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
-
-printf "%s\n" "" '# default python3' 'alias python=python3' >> $HOME/.bash_profile
