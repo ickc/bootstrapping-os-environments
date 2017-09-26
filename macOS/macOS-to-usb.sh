993145eea82083810cada8cc37b3bf19905d8d14
Kolen Cheung <christian.kolen@gmail.com>
2017-09-26 00:58:23 -0700

    macOS-to-usb.sh: fix syntax

--- macOS/macOS-to-usb.sh
+++ macOS/macOS-to-usb.sh
@@ -3,4 +3,4 @@
 # example
 # ./macOS-to-usb.sh /Applications/Install\ macOS\ High\ Sierra.app /Volumes/Install\ macOS\ High\ Sierra 
 
-sudo "'$1/Contents/Resources/createinstallmedia'" --volume "'$2'" --applicationpath "'$1'"
+sudo "$1/Contents/Resources/createinstallmedia" --volume "$2" --applicationpath "$1"
