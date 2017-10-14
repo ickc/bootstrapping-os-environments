925340bd8218a6fd01af76622532c276588b7bb5
Kolen Cheung <christian.kolen@gmail.com>
2017-10-14 16:53:06 -0700

    mas.sh: no longer necessary to migrate MMDC license

--- macOS/mas.sh
+++ macOS/mas.sh
@@ -4,4 +4,3 @@ grep -v '#' mas.txt | xargs mas install &&
 
 # open applications to transfer license to non-mas version
 open -a /Applications/aText.app
-open -a /Applications/MultiMarkdown\ Composer.app
