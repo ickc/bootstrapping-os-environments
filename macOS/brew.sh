a512b087e448de1d8848d4cf4ca30b70e3add615
Kolen Cheung <christian.kolen@gmail.com>
2017-10-14 16:51:01 -0700

    brew.sh: fix syntax

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -5,4 +5,4 @@ brew install mpich --build-from-source &&
 
 grep -v '#' brew.txt | xargs brew install &&
 
-printf "%s\n" "" '# Ruby from homebrew'' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
+printf "%s\n" "" '# Ruby from homebrew' 'export PATH=$(brew --prefix ruby)/bin:$PATH' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
