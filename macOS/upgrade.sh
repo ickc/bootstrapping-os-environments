f7121572faabe252086b55eabe73f4709676dc50
Kolen Cheung <christian.kolen@gmail.com>
2017-10-14 19:06:32 -0700

    upgrade.sh: major cleanup and remove pip

--- macOS/upgrade.sh
+++ macOS/upgrade.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
-# upgrade npm
-npm install npm -g
-# upgrade npm packages
-npm update -g
-
-# upgrade pip
-pip install -U pip setuptools # setuptools should be installed by default
-pip3 install -U pip setuptools # setuptools wheel should be installed by default
-# upgrade pip packages
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+# upgrade npm & npm packages
+npm install npm -g && npm update -g
 
 # upgrade brew packages (not including brew-cask)
-brew update
-brew upgrade
-brew cleanup
-brew doctor
+brew update && brew upgrade && brew cleanup && brew doctor
 
 # cleanup downloaded files (cask do not support upgrade yet)
 brew cask cleanup
@@ -25,8 +13,7 @@ brew cask cleanup
 mas upgrade
 
 # upgrade gems
-update_rubygems
-gem update --system
+update_rubygems && gem update --system
 
 # update cabal
 cabal update
