35ddf57c39b64181da15f717c31d64fb72f4710f
Kolen <christian.kolen@gmail.com>
2017-10-22 19:37:55 -0700

    npm.sh: remove adding mathjax-node to PATH
    Newer version of mathjax-node no longer has a bin

--- common/npm.sh
+++ common/npm.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 grep -v '#' npm.txt | xargs npm install -g
-
-printf "%s\n" "" '# MathJax-node' 'export PATH=$(dirname $(readlink -f $(which npm)))/../../mathjax-node/bin:$PATH' >> $HOME/.bash_profile
