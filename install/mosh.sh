d0fea4c9fecd52f46b297ac4b9607e7969234f3b
Kolen Cheung <christian.kolen@gmail.com>
2017-11-06 16:35:30 -0800

    mosh.sh: add prefix and NERSC-specific commands
    It doesn't compile on NERSC yet

--- install/mosh.sh
+++ install/mosh.sh
@@ -3,9 +3,15 @@
 version=1.3.2
 
 mkdir -p $HOME/.mosh &&
-cd $HOME.mosh &&
-wget -O - https://mosh.org/mosh-$version.tar.gz | tar -xvzf - &&
+cd $HOME/.mosh &&
+wget -qO- https://mosh.org/mosh-$version.tar.gz | tar -xzf - &&
 cd mosh-$version &&
-./configure &&
+if [[ -n $NERSC_HOST ]]; then
+	module swap PrgEnv-intel PrgEnv-gnu
+	module load protobuf/2.6.1 &&
+	protobuf_CFLAGS="-I$C_INCLUDE_PATH" protobuf_LIBS="-L$LD_LIBRARY_PATH" CC=cc CXX=CC ./configure
+else
+	./configure
+fi &&
 make &&
-make install
+make install --prefix="$HOME/.mosh"
