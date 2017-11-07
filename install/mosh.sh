0da9dfa91c2672bc1d076f0a86f2599fbdcc50e2
Kolen Cheung <christian.kolen@gmail.com>
2017-11-06 22:04:37 -0800

    mosh.sh: refactor and cleanup

--- install/mosh.sh
+++ install/mosh.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
-version=1.3.2
+# install mosh
 
-mkdir -p $HOME/.mosh &&
-cd $HOME/.mosh &&
-wget -qO- https://mosh.org/mosh-$version.tar.gz | tar -xzf - &&
-cd mosh-$version &&
+# optionally override these with env. var.
+TEMPDIR="${TEMPDIR-"$HOME/.mosh"}"
+P=${P-$(getconf _NPROCESSORS_ONLN)}
+VERSION=${VERSION-1.3.2}
+PREFIX="${PREFIX-"$HOME/.local"}"
+
+mkdir -p "$TEMPDIR" &&
+cd "$TEMPDIR" &&
+wget -qO- https://mosh.org/mosh-$VERSION.tar.gz | tar -xzf - &&
+cd mosh-$VERSION &&
 if [[ -n $NERSC_HOST ]]; then
-	module swap PrgEnv-intel PrgEnv-gnu
+	module swap PrgEnv-intel PrgEnv-gnu &&
 	module load protobuf/2.6.1 &&
-	protobuf_CFLAGS="-I$C_INCLUDE_PATH" protobuf_LIBS="-L$LD_LIBRARY_PATH" CC=cc CXX=CC ./configure
+	protobuf_CFLAGS="-I$C_INCLUDE_PATH" protobuf_LIBS="-L$LD_LIBRARY_PATH" CC=cc CXX=CC ./configure --prefix="$PREFIX" || exit 1
 else
-	./configure
-fi &&
-make &&
-make install --prefix="$HOME/.mosh"
+	./configure --prefix="$PREFIX" || exit 1
+fi
+
+make -j $P &&
+make install -j $P
+
+rm -rf "$TEMPDIR" &&
+
+echo '# export these PATH'
+echo export PATH="$PREFIX/bin:\$PATH"
+echo export MANPATH="$PREFIX/share/man:\$MANPATH"
