b6d7cabc7e8a673dc2eb0e1ed194a29fe7486f61
Kolen Cheung <christian.kolen@gmail.com>
2017-11-06 21:44:00 -0800

    mpibash.sh: seperate TEMPDIR and PREFIX; rm TEMPDIR

--- install/mpibash.sh
+++ install/mpibash.sh
@@ -3,10 +3,11 @@
 # install mpibash
 
 # optionally override these with env. var.
-BINDIR="${BINDIR-"$HOME/.mpibash"}"
+TEMPDIR="${TEMPDIR-"$HOME/.mpibash"}"
 P=${P-$(getconf _NPROCESSORS_ONLN)}
 BASHVERSION=${BASHVERSION-4.4.12}
 MPIBASHVERSION=${MPIBASHVERSION-master}
+PREFIX="${PREFIX-"$HOME/.local"}"
 
 # choose compiler
 if [[ -n $NERSC_HOST ]]; then
@@ -14,15 +15,16 @@ if [[ -n $NERSC_HOST ]]; then
 	# module swap PrgEnv-intel PrgEnv-gnu || exit 1
 fi
 
-mkdir -p "$BINDIR" &&
+mkdir -p "$TEMPDIR" &&
 
-cd "$BINDIR" &&
+cd "$TEMPDIR" &&
 wget -qO- https://ftp.gnu.org/gnu/bash/bash-$BASHVERSION.tar.gz | tar -xzf - &&
 cd bash-$BASHVERSION &&
-./configure &&
+./configure --prefix="$PREFIX" &&
 make -j $P || exit 1
+make install -j $P || exit 1
 
-cd "$BINDIR" &&
+cd "$TEMPDIR" &&
 if [[ $MPIBASHVERSION != master ]]; then
 	wget -qO- https://github.com/lanl/MPI-Bash/releases/download/v$MPIBASHVERSION/mpibash-$MPIBASHVERSION.tar.gz | tar -xzf - || exit 1
 	mpibashDir=mpibash-$MPIBASHVERSION
@@ -35,9 +37,12 @@ cd $mpibashDir &&
 if [[ $MPIBASHVERSION == master ]]; then
 	autoreconf -fvi || exit 1
 fi
-./configure --with-bashdir="$BINDIR/bash-$BASHVERSION" --prefix="$BINDIR/$mpibashDir" CC=cc &&
+./configure --with-bashdir="$TEMPDIR/bash-$BASHVERSION" --prefix="$PREFIX" CC=cc &&
 make -j $P &&
 make install -j $P &&
 
-echo '# export this PATH'
-echo export PATH="$BINDIR/bash-$BASHVERSION:$BINDIR/$mpibashDir/bin:\$PATH"
+rm -rf "$TEMPDIR" &&
+
+echo '# export these PATH'
+echo export PATH="$PREFIX/bin:\$PATH"
+echo export MANPATH="$PREFIX/share/man:\$MANPATH"
