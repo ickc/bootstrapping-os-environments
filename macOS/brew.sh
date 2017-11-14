b5f96d46872f83a4b85a67eaa4455f09216b464f
Kolen Cheung <christian.kolen@gmail.com>
2017-11-14 04:52:32 -0800

    brew.sh: fix syntax

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -3,4 +3,4 @@
 # only when build from source, it will uses homebrew's compiler (chosen as gcc from gnuize)
 brew install mpich --build-from-source &&
 
-grep -v '#' brew.txt | xargs brew install &&
+grep -v '#' brew.txt | xargs brew install
