918a5354e92ffbceb5bdad65e728d58c836d5d84
Kolen <christian.kolen@gmail.com>
2018-10-19 01:29:15 -0700

    jupyterlab.sh: require pyzmq>=17
    https://github.com/jupyter/notebook/issues/3595#issuecomment-386862260

--- common/conda/jupyterlab.sh
+++ common/conda/jupyterlab.sh
@@ -5,7 +5,7 @@ set -e
 path2txt="$(dirname "${BASH_SOURCE[0]}")/jupyterlab.txt"
 
 # create environment jupyterlab for using in jupyterhub
-conda create -n jupyterlab -c intel python=3 jupyterlab jupyterhub jupyterthemes -y
+conda create -n jupyterlab -c intel python=3 jupyterlab jupyterhub jupyterthemes 'pyzmq>=17' -y
 . activate jupyterlab
 
 temp=$(grep -v '#' "$path2txt")
