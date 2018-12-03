e1d0c4eb0494829a351b24592ab51afc80a6f955
Kolen <christian.kolen@gmail.com>
2018-12-02 19:54:39 -0800

    deb.sh: add windscribe

--- ubuntu/deb.sh
+++ ubuntu/deb.sh
@@ -8,3 +8,7 @@ wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && \
 # dropbox
 wget https://linux.dropbox.com/packages/ubuntu/dropbox_2015.10.28_amd64.deb && \
 	sudo dpkg -i dropbox_2015.10.28_amd64.deb
+# windscribe
+sudo apt install resolvconf -y
+wget https://windscribe.com/install/desktop/linux_deb_x64 && \
+	sudo dpkg -i linux_deb_x64
