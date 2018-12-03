ded69b404c66355c1e747448bdc01c2d2bbc4b01
Kolen <christian.kolen@gmail.com>
2018-12-02 20:03:12 -0800

    deb.sh: update to use apt install; update links

--- ubuntu/deb.sh
+++ ubuntu/deb.sh
@@ -3,12 +3,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Gitkraken
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb && \
-	sudo dpkg -i gitkraken-amd64.deb
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb &&
+	sudo apt install ./gitkraken-amd64.deb
 # dropbox
-wget https://linux.dropbox.com/packages/ubuntu/dropbox_2015.10.28_amd64.deb && \
-	sudo dpkg -i dropbox_2015.10.28_amd64.deb
+wget 'https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2018.11.28_amd64.deb' -O dropbox_amd64.deb &&
+	sudo apt install ./dropbox_amd64.deb
 # windscribe
-sudo apt install resolvconf -y
-wget https://windscribe.com/install/desktop/linux_deb_x64 && \
-	sudo dpkg -i linux_deb_x64
+wget https://windscribe.com/install/desktop/linux_deb_x64 -O windscribe_linux_deb_x64.deb &&
+	sudo apt install ./windscribe_linux_deb_x64.deb
