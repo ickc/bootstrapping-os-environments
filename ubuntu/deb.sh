9c0f1b764bcdf42eae2937b0c176ee497ac6b631
Kolen <christian.kolen@gmail.com>
2018-12-03 01:28:51 -0800

    deb.sh: add -y

--- ubuntu/deb.sh
+++ ubuntu/deb.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-export DEBIAN_FRONTEND=noninteractive
-
 # Gitkraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb &&
-	sudo apt install ./gitkraken-amd64.deb
+	sudo apt install ./gitkraken-amd64.deb -y
 # dropbox
 wget 'https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2018.11.28_amd64.deb' -O dropbox_amd64.deb &&
-	sudo apt install ./dropbox_amd64.deb
+	sudo apt install ./dropbox_amd64.deb -y
 # windscribe
 wget https://windscribe.com/install/desktop/linux_deb_x64 -O windscribe_linux_deb_x64.deb &&
-	sudo apt install ./windscribe_linux_deb_x64.deb
+	sudo apt install ./windscribe_linux_deb_x64.deb -y
