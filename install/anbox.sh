7e2f1b4f01b7333153ab8b058d058ec18182c120
Kolen <christian.kolen@gmail.com>
2018-12-14 03:51:50 -0800

    anbox.sh: add anbox-playstore-installer

--- install/anbox.sh
+++ install/anbox.sh
@@ -18,3 +18,16 @@ sudo snap install --devmode --beta anbox
 
 # debug
 snap info anbox
+
+# Add Google Play and ARM support
+mkdir -p ~/git/geeks-r-us && cd ~/git/geeks-r-us
+git clone git@github.com:geeks-r-us/anbox-playstore-installer.git
+cd anbox-playstore-installer
+
+sudo apt install wget lzip unzip squashfs-tools -y
+sed -i 's/^OPENGAPPS_RELEASEDATE.*$/OPENGAPPS_RELEASEDATE="20181214"/' install-playstore.sh
+sudo ./install-playstore.sh
+
+anbox.appmgr
+
+echo 'Enable all: Settings > Apps > Google Play Services/Store > Permissions'
