42856c49204a4f73341d98b2171ef5727b3cb2b3
Kolen <christian.kolen@gmail.com>
2018-12-28 23:50:42 -0800

    surface-linux-sign.sh: keep the unsigned ones

--- install/surface-linux-sign.sh
+++ install/surface-linux-sign.sh
@@ -10,6 +10,6 @@ sudo mokutil --list-enrolled
 cd ~
 
 find /boot -maxdepth 1 -name 'vmlinuz-*-surface+' -exec bash -c 'sudo sbsign --key MOK.priv --cert MOK.pem "$0" --output "$0.signed" && sudo rm -f "$0"' {} \;
-find /boot -maxdepth 1 -name 'initrd.img-*-surface+' -exec bash -c 'sudo mv "$0" "$0.signed"' {} \;
+find /boot -maxdepth 1 -name 'initrd.img-*-surface+' -exec bash -c 'sudo mv "$0.signed" "$0"' {} \;
 
 sudo update-grub
