b95600e37ffab1e7f00105ce64680bb3b71589ea
Kolen Cheung <christian.kolen@gmail.com>
2019-02-02 22:39:50 -0800

    brew-cash*.sh: migrate from caskroom/* to homebrew/cask-*

--- macOS/brew-cask-fonts.sh
+++ macOS/brew-cask-fonts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # the later for Microsoft Fonts
-brew tap caskroom/fonts &&
+brew tap homebrew/cask-fonts  &&
 brew tap niksy/pljoska &&
 
 grep -v '#' brew-cask-fonts.txt | xargs brew cask install
