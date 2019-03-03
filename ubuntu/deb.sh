6f7b23f7d339525138cfb64fc3781efe43449826
Kolen Cheung <christian.kolen@gmail.com>
2019-03-02 16:22:17 -0800

    ubuntu: use deb from github rather than apt

--- ubuntu/deb.sh
+++ ubuntu/deb.sh
@@ -7,4 +7,9 @@ wget 'https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2018.11.28_amd
 wget https://windscribe.com/install/desktop/linux_deb_x64 -O windscribe_linux_deb_x64.deb &&
 	sudo apt install ./windscribe_linux_deb_x64.deb -y
 
+# zero-tier
 curl -s https://install.zerotier.com/ | sudo bash
+
+# pandoc
+wget https://github.com/jgm/pandoc/releases/download/2.6/pandoc-2.6-1-amd64.deb -O pandoc.deb &&
+	sudo apt install ./pandoc.deb -
