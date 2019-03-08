bcb1b7efa9d762b11b4c8262a08e5ff47c04b252
Kolen Cheung <christian.kolen@gmail.com>
2019-03-08 15:14:00 -0800

    jupyterlab.sh: install from defaults channel for stability

--- common/conda/jupyterlab.sh
+++ common/conda/jupyterlab.sh
@@ -5,7 +5,7 @@ set -e
 path2txt="$(dirname "${BASH_SOURCE[0]}")/jupyterlab.txt"
 
 # create environment jupyterlab for using in jupyterhub
-conda create -n jupyterlab -c intel python=3 jupyterlab jupyterhub jupyterthemes 'pyzmq>=17' nodejs -y
+conda create -n jupyterlab -c defaults python=3 jupyterlab jupyterhub jupyterthemes 'pyzmq>=17' nodejs -y
 . activate jupyterlab
 
 temp=$(grep -v '#' "$path2txt")
