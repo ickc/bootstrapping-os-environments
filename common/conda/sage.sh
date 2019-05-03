5fb5aaea57dea8c612e94f4ea9fb3836d8ad340c
Kolen Cheung <christian.kolen@gmail.com>
2019-05-02 17:09:51 -0700

    rewrite conda/sage*

--- common/conda/sage.sh
+++ common/conda/sage.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-NAME="${NAME:-sage}"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-conda create -n "$NAME" intelpython3_core 'python>=3' sage -y
+conda env create -f "$DIR/sage.yml" -f
 
-. activate "$NAME"
+. activate sage
 
 # sagemath kernel
 jupyter kernelspec install --user $(dirname $(which sage))/../share/jupyter/kernels/sagemath
 
 # ipython kernel
-python -m ipykernel install --user --name "$NAME"
+python -m ipykernel install --user --name sage
---
+++ common/conda/sage3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+conda env create -f "$DIR/sage3.yml" -f
+
+. activate sage3
+
+# sagemath kernel
+jupyter kernelspec install --user $(dirname $(which sage))/../share/jupyter/kernels/sagemath --name sagemath3
+
+# ipython kernel
+python -m ipykernel install --user --name sage3
