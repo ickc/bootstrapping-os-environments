e0a744203ee210154de99f460d77cebfbf097998
Kolen Cheung <christian.kolen@gmail.com>
2019-05-03 18:45:41 -0700

    class.sh: add NPROC; use pip instead of python setup.py

--- install/class.sh
+++ install/class.sh
@@ -18,6 +18,7 @@ else
     defaultDir="$HOME/git/fork"
 fi
 INSTALLDIR="${INSTALLDIR:-$defaultDir}"
+NPROC="${NPROC:-$(nproc)}"
 mkdir -p "$INSTALLDIR"
 
 # obtain class
@@ -29,14 +30,14 @@ cd class
 make clean
 
 if [[ $(uname) == Darwin ]]; then
-    make -j$(nproc) OPTFLAG='-Ofast -ffast-math -march=native' CC=gcc-8
+    make -j$NPROC OPTFLAG='-Ofast -ffast-math -march=native' CC=gcc-8
 else
-    make -j$(nproc) OPTFLAG='-Ofast -ffast-math -march=native'
+    make -j$NPROC OPTFLAG='-Ofast -ffast-math -march=native'
 fi
 
 # install in all conda environments that ends in -defaults or -intel
 cd python
 for i in $(grep -E -- '-(defaults|intel)' ~/.conda/environments.txt); do
     . activate $i
-    python setup.py install
+    pip install .
 done
