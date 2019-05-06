fbc9cabbd18da4a70725c48d57e43a428531c315
Kolen <christian.kolen@gmail.com>
2019-05-06 14:04:39 -0700

    code.sh: use vscode insiders instead

--- common/code.sh
+++ common/code.sh
@@ -5,4 +5,4 @@ set -e
 path2txt="$(dirname "${BASH_SOURCE[0]}")/code.txt"
 
 temp=$(grep -v '#' "$path2txt")
-echo $temp | xargs -n1 code --install-extension
+echo $temp | xargs -n1 code-insiders --install-extension
