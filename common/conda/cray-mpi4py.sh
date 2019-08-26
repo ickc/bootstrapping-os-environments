c04c3fcb191c11cabe3259b2a700df099e31b474
Kolen <christian.kolen@gmail.com>
2019-08-26 14:40:15 -0700

    cray-mpi4py.sh: in non NERSC host use the simplest setup.py commands

--- common/conda/cray-mpi4py.sh
+++ common/conda/cray-mpi4py.sh
@@ -15,12 +15,13 @@ if [[ -n $NERSC_HOST ]]; then
     module swap PrgEnv-intel PrgEnv-gnu
     module unload craype-hugepages2M
     module unload libfabric || true
-fi
 
-python setup.py build --mpicc="$(which cc) -shared"
-python setup.py build_exe --mpicc="$(which cc) -dynamic"
-python setup.py install
-python setup.py install_exe
+    python setup.py build --mpicc="$(which cc) -shared" && python setup.py install
+    python setup.py build_exe --mpicc="$(which cc) -dynamic" && python setup.py install_exe
+else
+    # on lpc, mpi4py from conda's intel channel is used
+    python setup.py build && python setup.py install
+fi
 
 cd -
 
