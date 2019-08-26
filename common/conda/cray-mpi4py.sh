93fba2ff078744119a3efcb7dacfd9563dbc834f
Kolen <christian.kolen@gmail.com>
2019-08-26 13:48:52 -0700

    cray-mpi4py.sh: use module only when on NERSC_HOST

--- common/conda/cray-mpi4py.sh
+++ common/conda/cray-mpi4py.sh
@@ -11,9 +11,11 @@ mkdir -p "$tempDir" && cd "$tempDir"
 wget -qO- https://bitbucket.org/mpi4py/mpi4py/downloads/$mpiName.tar.gz | tar -xzf -
 cd $mpiName
 
-module swap PrgEnv-intel PrgEnv-gnu
-module unload craype-hugepages2M
-module unload libfabric || true
+if [[ -n $NERSC_HOST ]]; then
+    module swap PrgEnv-intel PrgEnv-gnu
+    module unload craype-hugepages2M
+    module unload libfabric || true
+fi
 
 python setup.py build --mpicc="$(which cc) -shared"
 python setup.py build_exe --mpicc="$(which cc) -dynamic"
