62bec989fc442365c2b24617d0bb528604a3468e
Kolen <christian.kolen@gmail.com>
2019-08-29 18:28:26 -0700

    upgrade.sh: remove legacy cask cleanup

--- macOS/upgrade.sh
+++ macOS/upgrade.sh
@@ -6,9 +6,6 @@ npm install npm -g && npm update -g
 # upgrade brew packages (not including brew-cask)
 brew update && brew upgrade && brew cleanup && brew doctor
 
-# cleanup downloaded files (cask do not support upgrade yet)
-brew cask cleanup
-
 # upgrade Mac App Store apps
 mas upgrade
 
