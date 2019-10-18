72416946fc28dc335e5c310044a8ce85626b249c
Kolen Cheung <christian.kolen@gmail.com>
2019-10-18 16:16:09 -0700

    cray-mpi4py.sh: fix a bug probably caused by new conda

--- common/conda/cray-mpi4py.sh
+++ common/conda/cray-mpi4py.sh
@@ -22,8 +22,41 @@ if [[ -n $NERSC_HOST ]]; then
     # also, https://docs.nersc.gov/programming/libraries/hdf5/h5py/
     # python setup.py build_exe --mpicc="$(which cc) -dynamic" && python setup.py install_exe
 else
-    # on lpc, mpi4py from conda's intel channel is used
-    python setup.py build && python setup.py install
+    # * see https://bitbucket.org/mpi4py/mpi4py/issues/143/build-failure-with-openmpi
+    cat << EOF > conf/sysconfigdata-conda-user.py
+import os, sysconfig
+from distutils.util import split_quoted
+
+_key = '_PYTHON_SYSCONFIGDATA_NAME'
+_val = os.environ.pop(_key, None)
+_name = sysconfig._get_sysconfigdata_name(True)
+
+_data = __import__(_name, globals(), locals(), ['build_time_vars'], 0)
+build_time_vars = _data.build_time_vars
+
+def _fix_options(opt):
+    if not isinstance(opt, str):
+        return opt
+    try:
+        opt = split_quoted(opt)
+    except:
+        return opt
+    try:
+        i = opt.index('-B')
+        del opt[i:i+2]
+    except ValueError:
+        pass
+    try:
+        i = opt.index('-Wl,--sysroot=/')
+        del opt[i]
+    except ValueError:
+        pass
+    return " ".join(opt)
+
+for _key in build_time_vars:
+    build_time_vars[_key] = _fix_options(build_time_vars[_key])
+EOF
+    _PYTHON_SYSCONFIGDATA_NAME=sysconfigdata-conda-user python setup.py build && python setup.py install
 fi
 
 cd -
