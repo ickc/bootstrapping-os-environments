e9d3fa06c1edbbcbc3b8b2984a6fd12a07f781d6
Kolen Cheung <kolen@dell-3650.hsd1.ca.comcast.net>
2019-11-10 23:22:25 -0800

    macOS/install.sh: install homebrew at ~/.homebrew and add port

--- macOS/install.sh
+++ macOS/install.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+# TODO: use heredoc to define bash functions ml_brew, ml_port
 
 # install xcode
 # the first time it runs, the process ends here
@@ -7,8 +9,29 @@ xcode-select --install
 
 sudo xcodebuild -license accept
 
+# Prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# sman
+mkdir -p ~/.local/bin
+bash -c "$(curl https://raw.githubusercontent.com/ickc/sman/master/install.sh)"
+
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
+mkdir ~/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew &&
+
+export PATH="$HOME/.homebrew/bin:$PATH"
+
+printf "%s\n" "" '# homebrew' 'export PATH="$HOME/.homebrew/bin:$PATH"' >> $HOME/.bash_profile
+
+# port
+wget https://distfiles.macports.org/MacPorts/MacPorts-2.6.2-10.15-Catalina.pkg
+sudo installer -pkg MacPorts-2.6.2-10.15-Catalina.pkg -target /
+rm -f MacPorts-2.6.2-10.15-Catalina.pkg
+printf "%s\n" "" '# port' 'export PATH="/opt/local/bin:$PATH"' >> $HOME/.bash_profile
 
 # mas
 brew install mas
@@ -16,18 +39,18 @@ brew install mas
 mas install 497799835
 
 # GNU-ize
-../submodule/gnuize.sh
+# TODO ../submodule/gnuize.sh
+../../GNU-ize/gnuize.sh -p ~/.homebrew
 
 # install node, npm
 brew install node ruby
 
-printf "%s\n" "" '# Ruby from homebrew' 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
+printf "%s\n" "" '# Ruby from homebrew' 'export PATH="~/.homebrew/opt/ruby/bin:$PATH"' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
 
 gem install rubygems-update
 
 # first 2 needed by closure-compiler, sshfs respectively
 brew cask install java osxfuse anaconda
 
-# conda PATH
-# put it to the end that expose activate but not override others
-printf "%s\n" "" "# conda" 'PATH="$PATH:/usr/local/anaconda3/bin"' >> $HOME/.bash_profile
+# conda
+~/.homebrew/anaconda3/bin/conda init zsh
