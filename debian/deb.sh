91e8ac2fb4dbd97df27c83ed80c6b640c7eced9d
Kolen Cheung <christian.kolen@gmail.com>
2020-01-27 02:04:29 -0800

    deb.sh: remove dropbox; update pandoc

--- debian/deb.sh
+++ debian/deb.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# dropbox
-wget 'https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2018.11.28_amd64.deb' -O dropbox_amd64.deb &&
-	sudo apt install ./dropbox_amd64.deb -y
 # windscribe
 wget https://windscribe.com/install/desktop/linux_deb_x64 -O windscribe_linux_deb_x64.deb &&
 	sudo apt install ./windscribe_linux_deb_x64.deb -y
@@ -11,5 +8,5 @@ wget https://windscribe.com/install/desktop/linux_deb_x64 -O windscribe_linux_de
 curl -s https://install.zerotier.com/ | sudo bash
 
 # pandoc
-wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb -O pandoc.deb &&
+wget https://github.com/jgm/pandoc/releases/download/2.9.1.1/pandoc-2.9.1.1-1-amd64.deb -O pandoc.deb &&
 	sudo apt install ./pandoc.deb -y
