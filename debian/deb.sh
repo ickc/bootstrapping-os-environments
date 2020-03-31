062ddd0d5f995f46e509ee732d4b7a6e2421213d
Kolen <christian.kolen@gmail.com>
2020-03-30 18:05:32 -0700

    Update deb.sh

--- debian/deb.sh
+++ debian/deb.sh
@@ -6,7 +6,7 @@ mkdir -p deb
 
 # windscribe
 wget https://windscribe.com/install/desktop/linux_deb_x64 -O deb/windscribe_linux_deb_x64.deb
-sudo apt install deb/windscribe_linux_deb_x64.deb -y
+sudo apt install ./deb/windscribe_linux_deb_x64.deb -y
 
 # zero-tier
 curl -s https://install.zerotier.com/ | sudo bash
@@ -15,7 +15,7 @@ curl -s https://install.zerotier.com/ | sudo bash
 downloadUrl="https://github.com$(curl -L https://github.com/jgm/pandoc/releases/latest | grep -o '/jgm/pandoc/releases/download/.*-amd64\.deb')"
 
 wget "$downloadUrl" -O deb/pandoc.deb
-sudo apt install deb/pandoc.deb -y
+sudo apt install ./deb/pandoc.deb -y
 
 tree deb
 echo Cleaning up...
