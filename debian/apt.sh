709a1e675c22cf4b94fb20416b76a4ae146340c3
Kolen Cheung <christian.kolen@gmail.com>
2020-04-24 11:01:02 -0700

    apt.sh: add kodi ppa

--- debian/apt.sh
+++ debian/apt.sh
@@ -4,6 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update -qq && sudo apt -y full-upgrade
 
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+
 xargs -a <(sed -E 's/^([^# ]*).*$/\1/g' apt.txt) -r -- sudo apt-get install -qq
 
 sudo sensors-detect --auto
