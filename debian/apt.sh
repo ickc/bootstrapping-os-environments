b80ccab1965de137fa564e0b61169f70c7807774
Kolen Cheung <christian.kolen@gmail.com>
2020-04-25 18:43:17 -0700

    apt.*: add alacritty

--- debian/apt.sh
+++ debian/apt.sh
@@ -9,6 +9,7 @@ sudo apt -y full-upgrade
 
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:team-xbmc/ppa
+sudo add-apt-repository ppa:mmstick76/alacritty
 sudo apt-get update -qq
 
 xargs -a <(sed -E 's/^([^# ]*).*$/\1/g' apt.txt) -r -- sudo apt-get install -qq
