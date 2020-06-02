677e0b02a518afc81cabfa3d11ea7ea8253aca9e
Kolen Cheung <christian.kolen@gmail.com>
2020-06-01 19:41:12 -0700

    macOS/download.sh: cleanup

--- macOS/download.sh
+++ macOS/download.sh
@@ -3,20 +3,3 @@
 # post brew-cask.sh
 ## open Adobe CC installer downloaded by brew cask
 open -a "~/.homebrew/Caskroom/adobe-creative-cloud/latest/Creative Cloud Installer.app"
-
-# create temp folder for downloads
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-mkdir -p temp
-cd temp
-
-# Solarized Terminal
-git clone git://github.com/tomislav/osx-terminal.app-colors-solarized.git
-open 'osx-terminal.app-colors-solarized/Solarized Light.terminal'
-open 'osx-terminal.app-colors-solarized/Solarized Dark.terminal'
-
-# Safari extension
-wget http://cdn3.brettterpstra.com/instapaperbeyond/TabLinks.safariextz && open TabLinks.safariextz
-
-
-cd ..
-rm -rf temp
