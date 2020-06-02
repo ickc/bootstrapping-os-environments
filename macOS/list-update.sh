03a76eee03be3c2c016927f85651ea3499552cc4
Kolen Cheung <christian.kolen@gmail.com>
2020-06-01 19:20:53 -0700

    Update list-update.sh

--- macOS/list-update.sh
+++ macOS/list-update.sh
@@ -11,6 +11,11 @@ mas list | xargs -i -n1 bash -c 'cat install.sh mas.txt | if ! grep -q ${0%% *} 
 
 printline
 
+echo Consider add the following to port.txt:
+port installed requested | grep -oP '^  ([^@]*)' | xargs -i -n1 bash -c 'cat port.txt | if ! grep -q ${0%% *} -; then echo $0; fi' {}
+
+printline
+
 echo Consider add the following to brew.txt:
 brew leaves | xargs -i -n1 bash -c 'cat gnuize.sh install.sh brew.sh brew.txt | if ! grep -q $0 -; then echo $0; fi' {}
 
