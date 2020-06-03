ac445ce769fd12d0b3e2c530f6391b66d378cbc3
Kolen Cheung <christian.kolen@gmail.com>
2020-06-03 01:46:20 -0700

    brew.sh: remove mpich

--- macOS/brew.sh
+++ macOS/brew.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-# only when build from source, it will uses homebrew's compiler (chosen as gcc from gnuize)
-brew install mpich --build-from-source &&
-
 grep -v '#' brew.txt | xargs -n1 brew install
