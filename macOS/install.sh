72a67c00f3cfb626ed0f0635ad2a62c6dd6ad215
Kolen <christian.kolen@gmail.com>
2020-08-08 20:07:57 -0700

    macOS/install.sh: cleanup and update
    c.f. ickc/dotfiles

--- macOS/install.sh
+++ macOS/install.sh
@@ -11,25 +11,12 @@ xcode-select --install
 
 sudo xcodebuild -license accept
 
-# Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-# sman
-mkdir -p ~/.local/bin
-bash -c "$(curl https://raw.githubusercontent.com/ickc/sman/master/install.sh)"
-
 # install brew
 mkdir ~/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew &&
-
 export PATH="$HOME/.homebrew/bin:$PATH"
-printf "%s\n" "" '# homebrew' 'export PATH="$HOME/.homebrew/bin:$PATH"' >> $HOME/.bash_profile
 
-# port
-MACPORTS_VERSION=2.6.2
+# port: update from https://www.macports.org/install.php
+MACPORTS_VERSION=2.6.3
 MACPORTS_OS_VERSION=10.15-Catalina
 curl "https://distfiles.macports.org/MacPorts/MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" --output "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg"
 sudo installer -pkg "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" -target /
@@ -39,19 +26,11 @@ printf "%s\n" "" '# port' 'export PATH="/opt/local/bin:$PATH"' >> $HOME/.bash_pr
 # mas
 brew install mas
 
-# may be needed again after Xcode is installed?
-sudo xcodebuild -license accept
-
 # install node, npm
 brew install node ruby
-
 export PATH="~/.homebrew/opt/ruby/bin:$PATH"
-printf "%s\n" "" '# Ruby from homebrew' 'export PATH="~/.homebrew/opt/ruby/bin:$PATH"' >> $HOME/.bash_profile #[Small but very useful tip on using jekyll on macosx when you use hoembrew · Issue #1504 · jekyll/jekyll](https://github.com/jekyll/jekyll/issues/1504)
 
 gem install rubygems-update
 
-# first 2 needed by closure-compiler, sshfs respectively
-brew cask install java osxfuse anaconda
-
-# conda
-~/.homebrew/anaconda3/bin/conda init zsh
+# java needed by closure-compiler
+brew cask install java anaconda
