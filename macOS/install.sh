79f2d1dd112c43e79eebcf107b98126d274a93c6
Kolen Cheung <christian.kolen@gmail.com>
2020-11-12 22:51:26 -0800

    macOS/install.sh: remove xcode and add more stdout

--- macOS/install.sh
+++ macOS/install.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env zsh
 
-# TODO: use heredoc to define bash functions ml_brew, ml_port
-
 # sudo loop
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# install xcode command line tools
-xcode-select --install
+# helpers ##############################################################
 
+print_double_line () {
+    eval printf %.0s= '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
+
+print_line () {
+    eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
+
+########################################################################
+
+print_double_line
+echo "install homebrew..."
 # install brew
 mkdir ~/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew &&
 export PATH="$HOME/.homebrew/bin:$PATH"
 
+print_double_line
+echo "install macports..."
 # port: update from https://www.macports.org/install.php
 MACPORTS_VERSION=2.6.3
 MACPORTS_OS_VERSION=10.15-Catalina
@@ -20,16 +31,21 @@ curl "https://distfiles.macports.org/MacPorts/MacPorts-${MACPORTS_VERSION}-${MAC
 sudo installer -pkg "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" -target /
 rm -f "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg"
 
+print_double_line
+echo "install mas..."
 # mas
 brew install mas
-# macport needs xcode
-mas install 497799835
-sudo xcodebuild -license accept
 
+print_double_line
+echo "install node and ruby..."
 # install node, npm
 brew install node ruby
 export PATH="~/.homebrew/opt/ruby/bin:$PATH"
 
+print_line
+echo 'update gem...'
 gem install rubygems-update
 
+print_double_line
+echo 'install anaconda and oracle-jdk...'
 brew cask install anaconda oracle-jdk
