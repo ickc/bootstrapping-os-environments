6c261e31f1663f91c10fe2e6e1e448ef7c30ddb7
Kolen Cheung <christian.kolen@gmail.com>
2020-11-13 00:32:31 -0800

    install.sh: install macports from master

--- macOS/install.sh
+++ macOS/install.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+MACPORTS_VERSION=2.6.3
+MACPORTS_OS_VERSION=10.15-Catalina
+
 # sudo loop
 sudo xcodebuild -license accept
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
@@ -27,11 +30,22 @@ export PATH="$HOME/.homebrew/bin:$PATH"
 print_double_line
 echo "install macports..."
 # port: update from https://www.macports.org/install.php
-MACPORTS_VERSION=2.6.3
-MACPORTS_OS_VERSION=10.15-Catalina
-curl "https://distfiles.macports.org/MacPorts/MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" --output "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg"
-sudo installer -pkg "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" -target /
-rm -f "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg"
+# prebuild binaries
+# curl "https://distfiles.macports.org/MacPorts/MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" --output "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg"
+# sudo installer -pkg "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg" -target /
+# rm -f "MacPorts-${MACPORTS_VERSION}-${MACPORTS_OS_VERSION}.pkg"
+# build from source
+# curl https://distfiles.macports.org/MacPorts/MacPorts-${MACPORTS_VERSION}.tar.bz2 --output MacPorts-${MACPORTS_VERSION}.tar.bz2
+# tar xjvf MacPorts-${MACPORTS_VERSION}.tar.bz2
+# cd MacPorts-${MACPORTS_VERSION}
+# build from master
+git clone https://github.com/macports/macports-base.git
+cd macports-base
+./configure && make && sudo make install
+cd ..
+# rm -rf MacPorts-${MACPORTS_VERSION}*
+rm -rf macports-base
+sudo port -v selfupdate
 
 print_double_line
 echo "install mas..."
