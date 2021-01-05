a9e040b61eeda904f73dd6b32936ae032b8d71ff
Kolen Cheung <christian.kolen@gmail.com>
2021-01-05 15:35:12 -0800

    macOS/upgrade.sh: remove npm, gem

--- macOS/upgrade.sh
+++ macOS/upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # upgrade npm & npm packages
-npm install npm -g && npm update -g
+# npm install npm -g && npm update -g
 
 # upgrade brew packages (not including brew-cask)
 brew update && brew upgrade && brew cleanup && brew doctor
@@ -11,7 +11,7 @@ mas upgrade
 
 # upgrade gems
 # update_rubygems
-gem update --system
+# gem update --system
 
 # update cabal
 # cabal update
