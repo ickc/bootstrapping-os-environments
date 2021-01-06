7362d0cb1c233444f57cc56716696e4f0f8af75a
Kolen Cheung <christian.kolen@gmail.com>
2021-01-05 17:04:56 -0800

    port.sh: add git config

--- macOS/port.sh
+++ macOS/port.sh
@@ -5,3 +5,6 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 grep -v '#' port.txt | xargs sudo port install
+
+# configure git after macports's git is installed
+git config --global pull.rebase false
