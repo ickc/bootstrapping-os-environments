ea577178f427acb8ee3aebaade4d5cc9d92a8b46
Kolen Cheung <christian.kolen@gmail.com>
2021-01-21 20:52:52 -0800

    Create neofetch.sh

---
+++ install/neofetch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PREFIX=${PREFIX:-/global/common/software/polar/local}
+
+GH_SHORT=dylanaraps/neofetch
+
+url="https://github.com/$GH_SHORT/releases/latest"
+# downloadUrl="https://github.com$(curl -L $url | grep -o "/$GH_SHORT/releases/download/FILENAMEPATTERN")"
+downloadUrl="https://github.com$(curl -L $url | grep -o "/$GH_SHORT/archive/.*\.tar.gz")"
+filename="${downloadUrl##*/}"
+
+wget "$downloadUrl"
+
+tar -xf "$filename"
+
+cd neofetch-*
+
+export PREFIX
+make install
