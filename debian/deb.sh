e1ca8ab45531c697311210badef8a50d51aad8a7
Kolen Cheung <christian.kolen@gmail.com>
2021-01-21 23:51:27 -0800

    deb.sh: add sudo loop

--- debian/deb.sh
+++ debian/deb.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# sudo loop
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # helpers ##############################################################
 
 print_double_line () {
