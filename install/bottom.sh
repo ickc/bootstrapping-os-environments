2015920fe7e1bf588fa74910bfde77b3021620e8
Kolen Cheung <christian.kolen@gmail.com>
2021-01-21 23:49:55 -0800

    bottom.sh: fix grep downloadUrl

--- install/bottom.sh
+++ install/bottom.sh
@@ -5,7 +5,7 @@ set -e
 GH_SHORT=ClementTsang/bottom
 
 url="https://github.com/$GH_SHORT/releases/latest"
-downloadUrl="https://github.com$(curl -L $url | grep -o "/$GH_SHORT/releases/download/bottom_.*_amd64\.deb")"
+downloadUrl="https://github.com$(curl -L $url | grep -o "/$GH_SHORT/releases/download/.*/bottom.*amd64\.deb")"
 # downloadUrl="https://github.com$(curl -L $url | grep -o "/$GH_SHORT/archive/.*\.tar.gz")"
 filename="${downloadUrl##*/}"
 
