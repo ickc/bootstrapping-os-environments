e1c512b060f9e0067b588f3244f8d5f92fc1a976
Kolen Cheung <christian.kolen@gmail.com>
2021-01-21 23:42:47 -0800

    Create windscribe.sh

---
+++ install/windscribe.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+case "$(uname -sm)" in
+   Linux\ x86_64)  downloadUrl=https://windscribe.com/install/desktop/linux_deb_x64 ;;
+   Linux\ i*86)    downloadUrl=https://windscribe.com/install/desktop/linux_deb_x86 ;;
+   Linux\ arm*)    downloadUrl=https://windscribe.com/install/desktop/linux_deb_arm ;;
+   Linux\ aarch64) downloadUrl=https://windscribe.com/install/desktop/linux_deb_arm ;;
+esac
+
+wget "$downloadUrl" -O windscribe.deb
+
+sudo apt install ./windscribe.deb -y
+
+rm -f windscribe.deb
