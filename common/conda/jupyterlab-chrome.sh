14b8c1d41e6de3790aa4707d2d28d736ede15342
Kolen Cheung <christian.kolen@gmail.com>
2021-01-22 00:57:54 -0800

    jupyterlab-chrome.sh: add linux

--- common/conda/jupyterlab-chrome.sh
+++ common/conda/jupyterlab-chrome.sh
@@ -2,8 +2,16 @@
 
 . activate jupyterlab
 
-jupyter lab --generate-config
+: > ~/.jupyter/jupyter_notebook_config.py
 
+if [[ $(uname) == Darwin ]]; then
 cat << 'EOF' >> ~/.jupyter/jupyter_notebook_config.py
 c.LabApp.browser = '"/Applications/Chromium.app/Contents/MacOS/Chromium" --app=%s'
+c.NotebookApp.use_redirect_file = False
 EOF
+else
+cat << 'EOF' >> ~/.jupyter/jupyter_notebook_config.py
+c.LabApp.browser = 'chromium-browser --app=%s'
+c.NotebookApp.use_redirect_file = False
+EOF
+fi
