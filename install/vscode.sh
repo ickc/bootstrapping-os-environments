1feaf03f57da26b6f7621445aa4572e6cbf730e6
Kolen Cheung <christian.kolen@gmail.com>
2021-01-21 23:53:03 -0800

    vscode.sh: quote

--- install/vscode.sh
+++ install/vscode.sh
@@ -3,9 +3,15 @@
 set -e
 
 case "$(uname -sm)" in
-   Linux\ x86_64)  downloadUrl=https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 ;;
-   Linux\ arm*)    downloadUrl=https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-armhf ;;
-   Linux\ aarch64) downloadUrl=https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64 ;;
+  Linux\ x86_64)
+    downloadUrl='https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+    ;;
+  Linux\ arm*)
+    downloadUrl='https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-armhf'
+    ;;
+  Linux\ aarch64)
+    downloadUrl='https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64'
+    ;;
 esac
 
 wget "$downloadUrl" -O vscode.deb
