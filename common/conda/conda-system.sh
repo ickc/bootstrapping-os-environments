367e15afa5b62d989f0af980bd87e99a616dd2a0
Kolen Cheung <christian.kolen@gmail.com>
2021-01-22 17:49:49 -0800

    Update conda-system.sh

--- common/conda/conda-system.sh
+++ common/conda/conda-system.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+PREFIX=${PREFIX:-/global/common/software/polar/.conda/envs/system39-conda-forge}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 ./conda_env.py -o temp.yml -n system -C conda-system.txt -v 3.9 -c conda-forge
-mamba env create -f temp.yml -p /global/common/software/polar/.conda/envs/system39-conda-forge
+mamba env create -f temp.yml -p "$PREFIX"
 rm -f temp.yml
