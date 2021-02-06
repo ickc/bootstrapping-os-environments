c5dfd69e4491a864bf2dd55db958841c8d44c2ad
Kolen Cheung <christian.kolen@gmail.com>
2021-02-05 18:34:55 -0800

    jupyterlab-chrome.sh: use self-signed certificate

--- common/conda/jupyterlab-chrome.sh
+++ common/conda/jupyterlab-chrome.sh
@@ -3,14 +3,24 @@
 . activate jupyterlab
 
 mkdir -p ~/.jupyter
-: > ~/.jupyter/jupyter_lab_config.py
 
 if [[ $(uname) == Darwin ]]; then
-cat << 'EOF' >> ~/.jupyter/jupyter_lab_config.py
+cat << 'EOF' > ~/.jupyter/jupyter_lab_config.py
 c.LabApp.browser = '"/Applications/Chromium.app/Contents/MacOS/Chromium" -incognito --app=%s'
+c.ServerApp.iopub_data_rate_limit = 10000000000
 EOF
 else
-cat << 'EOF' >> ~/.jupyter/jupyter_lab_config.py
+cat << 'EOF' > ~/.jupyter/jupyter_lab_config.py
 c.LabApp.browser = 'chromium-browser -incognito --app=%s'
+c.ServerApp.iopub_data_rate_limit = 10000000000
 EOF
 fi
+
+# https://jupyterlab.readthedocs.io/en/stable/user/jupyterhub.html#jupyterhub
+cat << EOF > ~/.jupyter/jupyterhub_config.py
+c.Spawner.default_url = '/lab'
+c.JupyterHub.ssl_key = '$HOME/.jupyter/ssl.key'
+c.JupyterHub.ssl_cert = '$HOME/.jupyter/ssl.crt'
+EOF
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/.jupyter/ssl.key -out ~/.jupyter/ssl.crt
