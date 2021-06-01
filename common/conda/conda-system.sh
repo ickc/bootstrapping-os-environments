03265be1f061ee828abc8ed90ef17b5d3971211f
Kolen Cheung <christian.kolen@gmail.com>
2021-06-01 14:16:54 -0700

    add BINDIR

--- common/conda/conda-system.sh
+++ common/conda/conda-system.sh
@@ -3,6 +3,7 @@
 set -e
 
 PREFIX=${PREFIX:-/global/common/software/polar/.conda/envs/system39-conda-forge}
+BINDIR=${BINDIR:-/global/common/software/polar/local/bin}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
@@ -10,3 +11,7 @@ cd "$DIR"
 ./conda_env.py -o temp.yml -n system -C conda-system.txt -v 3.9 -c conda-forge
 mamba env create -f temp.yml -p "$PREFIX"
 rm -f temp.yml
+
+while read line; do
+    ln -s "$PREFIX/bin/$line" "$BINDIR"
+done < conda-system-link.txt
