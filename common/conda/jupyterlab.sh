b159c2769c389a2c2b576a04b50d7fe50452ab5e
Kolen Cheung <christian.kolen@gmail.com>
2021-07-16 19:34:56 -0700

    update if existed

--- common/conda/jupyterlab.sh
+++ common/conda/jupyterlab.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # create environment jupyterlab for using in jupyterhub
-mamba env create -f "$DIR/jupyterlab.yml"
+mamba env create -f "$DIR/jupyterlab.yml" || mamba env update -f "$DIR/jupyterlab.yml"
 . activate jupyterlab
 
 temp=($(grep -v '#' "$DIR/jupyterlab.txt"))
