cceddba48d6776f21c712d9d199335c261209457
Kolen Cheung <christian.kolen@gmail.com>
2021-08-17 17:04:17 -0700

    add plotly, jupyterlab-geojson, jupyterlab-katex; remove nodejs

--- common/conda/jupyterlab.sh
+++ common/conda/jupyterlab.sh
@@ -6,10 +6,3 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # create environment jupyterlab for using in jupyterhub
 mamba env create -f "$DIR/jupyterlab.yml" || mamba env update -f "$DIR/jupyterlab.yml"
-. activate jupyterlab
-
-temp=($(grep -v '#' "$DIR/jupyterlab.txt"))
-
-# https://plotly.com/python/getting-started/#jupyterlab-support-python-35
-export NODE_OPTIONS=--max-old-space-size=4096
-jupyter labextension install "${temp[@]}"
