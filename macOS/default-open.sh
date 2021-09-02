593139ba5d16fad5825912e9dd72211c6a072e9a
Kolen Cheung <christian.kolen@gmail.com>
2021-09-01 18:37:14 -0700

    add Rmd

--- macOS/default-open.sh
+++ macOS/default-open.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # https://github.com/mathiasbynens/dotfiles/issues/458
-for ext in toml dict inc "in" native cfg csl ipynb; do
+for ext in toml dict inc "in" native cfg csl ipynb Rmd; do
     defaults write ~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist LSHandlers -array-add \
     "{
         LSHandlerContentTag = ${ext};
