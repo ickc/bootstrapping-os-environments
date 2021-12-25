20f255683a855c74400da22104b2ea75381d1581
Kolen Cheung <christian.kolen@gmail.com>
2021-12-24 23:41:23 -0800

    Update mas.sh

--- macOS/mas.sh
+++ macOS/mas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-grep -v '#' mas.txt | xargs mas install &&
+grep -v '#' mas.txt | xargs mas install
 
 # open applications to transfer license to non-mas version
 # open -a /Applications/aText.app
