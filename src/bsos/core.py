998fd0984ba9c9cb7c7b937d0dc9f790a3581b8f
Kolen Cheung <christian.kolen@gmail.com>
2022-01-11 14:01:18 -0800

    use conda.models.match_spec.MatchSpec

--- src/bsos/core.py
+++ src/bsos/core.py
@@ -4,6 +4,47 @@ from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
 
+from conda.models.match_spec import MatchSpec
+
+
+@dataclass
+class Package:
+    """Parse a single line of the txt format used in specifying packages.
+
+    the convention is that
+
+    - starts with ``#`` is a package to be ignored in installation
+    - starts with ``#*`` is a comment
+    - optionally has `::` for conda channel delimiter and channel will be ignored here
+    - optionally has version pinned by `=` and will be ignored here
+    """
+
+    match_spec: MatchSpec
+    ignored: bool = False
+
+    @classmethod
+    def from_txt_line(cls, line: str) -> Package | None:
+        if line.startswith("#*"):
+            return None
+        ignored = False
+        if line.startswith("#"):
+            ignored = True
+            line = line.lstrip("#").strip()
+        match_spec = MatchSpec(line)
+        return cls(match_spec, ignored)
+
+    @property
+    def name(self) -> str:
+        return self.match_spec.name
+
+    @property
+    def version(self) -> str:
+        return "" if (temp := self.match_spec.version) is None else temp.spec
+
+    @property
+    def channel(self) -> str:
+        return m["channel"] if "channel" in (m := self.match_spec._match_components) else ""
+
 
 @dataclass
 class Config:
@@ -17,17 +58,13 @@ class Config:
     - optionally has version pinned by `=` and will be ignored here
     """
 
-    text: list[str]
+    packages: list[Package]
 
     @classmethod
     def from_file(cls, path: Path) -> Config:
         with open(path, "r") as f:
-            return cls(f.readlines())
+            return cls([p for line in f if (p := Package.from_txt_line(line.strip())) is not None])
 
     @cached_property
     def packages_including_ingored(self) -> set[str]:
-        return set(
-            package
-            for word in self.text
-            if (package := word.lstrip("#").strip().split("::")[-1].split("=")[0]) and not package.startswith("*")
-        )
+        return set(p.name for p in self.packages)
