332987759c8791999a312cc755565185ad3f02c0
Kolen Cheung <christian.kolen@gmail.com>
2022-01-13 16:52:45 -0800

    port -N: Non-interactive mode

--- macOS/port.sh
+++ macOS/port.sh
@@ -5,15 +5,15 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # setup compilers first
-sudo port install gcc10 mpich mpich-gcc10
-sudo port select --set gcc mp-gcc10
-sudo port select --set mpi mpich-gcc10
+sudo port -N install gcc10 mpich mpich-gcc10
+sudo port -N select --set gcc mp-gcc10
+sudo port -N select --set mpi mpich-gcc10
 
-grep -v '#' port.txt | xargs sudo port install
+grep -v '#' port.txt | xargs sudo port -N install
 
 # configure git after macports's git is installed
 git config --global pull.rebase false
 
-sudo port load smartmontools
-sudo port load openssh
-sudo port load rsync
+sudo port -N load smartmontools
+sudo port -N load openssh
+sudo port -N load rsync
