35801e5b28664c6013baff5d959fc1548b15e927
Kolen Cheung <christian.kolen@gmail.com>
2022-04-18 16:54:09 -0700

    add CFLAGS

--- install/libarchive.sh
+++ install/libarchive.sh
@@ -6,6 +6,7 @@ TEMPDIR="${TEMPDIR-"$SCRATCH/.libarchive"}"
 VERSION=${VERSION-3.6.1}
 # assume using a conda environment
 PREFIX="${PREFIX-"$CONDA_PREFIX"}"
+CFLAGS="-O3 -fPIC"
 
 # c.f. https://stackoverflow.com/a/23378780/5769446
 P="${P-$(if [[ "$(uname)" == Darwin ]]; then sysctl -n hw.physicalcpu_max; else lscpu -p | grep -E -v '^#' | sort -u -t, -k 2,4 | wc -l; fi)}"
@@ -15,7 +16,7 @@ mkdir -p "$TEMPDIR" &&
 cd "$TEMPDIR" &&
 wget -qO- http://www.libarchive.org/downloads/libarchive-$VERSION.tar.gz | tar -xzf - &&
 cd libarchive-$VERSION &&
-./configure --prefix=$PREFIX &&
+CFLAGS="$CFLAGS" ./configure --prefix=$PREFIX &&
 
 make -j $P &&
 make install -j $P
