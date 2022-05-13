03671f900ccf5d1e6fd95848c67892d134359bfc
Kolen Cheung <christian.kolen@gmail.com>
2022-05-12 20:04:39 -0700

    simplify and support Apple Silicon

--- install/mamba.sh
+++ install/mamba.sh
@@ -5,17 +5,7 @@ set -e
 CONDA_PREFIX="${CONDA_PREFIX:-"$HOME/.mambaforge"}"
 
 # https://github.com/conda-forge/miniforge
-case "$(uname -sm)" in
-  Darwin\ x86_64)
-    downloadUrl=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-x86_64.sh
-    ;;
-  Linux\ x86_64)
-    downloadUrl=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-    ;;
-  Linux\ aarch64)
-    downloadUrl=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-aarch64.sh
-    ;;
-esac
+downloadUrl="https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 
 curl -L "$downloadUrl" --location --output Mambaforge.sh
 
