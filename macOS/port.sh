e41b98160b714ef3e9ef51da4b68736f42d802a5
Kolen Cheung <christian.kolen@gmail.com>
2022-05-13 17:47:47 -0700

    upgrade to gcc11

--- macOS/port.sh
+++ macOS/port.sh
@@ -5,9 +5,9 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # setup compilers first
-sudo port -N install gcc10 mpich mpich-gcc10
-sudo port -N select --set gcc mp-gcc10
-sudo port -N select --set mpi mpich-gcc10
+sudo port -N install gcc11 mpich mpich-gcc11
+sudo port -N select --set gcc mp-gcc11
+sudo port -N select --set mpi mpich-gcc11
 
 grep -v '#' port.txt | xargs sudo port -N install
 
