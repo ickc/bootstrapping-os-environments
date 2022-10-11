943d0fe158d7236e41f665f70995d666cbb7a978
Kolen Cheung <christian.kolen@gmail.com>
2022-10-10 17:25:30 -0700

    Update key-remapping-hidutil.sh

--- macOS/key-remapping-hidutil.sh
+++ macOS/key-remapping-hidutil.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # modify from https://apple.stackexchange.com/a/398797/355318
 # also see https://developer.apple.com/library/archive/technotes/tn2450/_index.html
 # this maps the Application key used in Windows to the Right Alt key used in macOS
-cp -f com.local.KeyRemapping.plist ~/Library/LaunchAgents/
+cp -f "$DIR/com.local.KeyRemapping.plist" ~/Library/LaunchAgents/
