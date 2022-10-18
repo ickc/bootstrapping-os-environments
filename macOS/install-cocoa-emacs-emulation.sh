f95473a7c4344f4f05024169cec41317cd64981b
Kolen Cheung <christian.kolen@gmail.com>
2022-10-17 20:04:17 -0700

    Update install-cocoa-emacs-emulation.sh

--- macOS/install-cocoa-emacs-emulation.sh
+++ macOS/install-cocoa-emacs-emulation.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://web.archive.org/web/20220608191954/http://www.hcs.harvard.edu/~jrus/site/cocoa-text.html
+
 TARGETDIR="$HOME/Library/KeyBindings/"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
