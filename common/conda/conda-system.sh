694ad81536ee27ec1c9cf1f92a56b7c544ed4cef
Kolen Cheung <christian.kolen@gmail.com>
2023-04-25 20:28:20 +0000

    add UPDATE flag

--- common/conda/conda-system.sh
+++ common/conda/conda-system.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+# * use UPDATE=1 to update the environment instead
+
 # * Define PREFIX if you want to install in a conda prefix instead
 # PREFIX=
 BINDIR=${BINDIR:-~/.local/bin}
@@ -12,11 +14,19 @@ cd "$DIR"
 ./../../src/bsos/conda_env.py -o temp.yml -n system -C conda-system.txt -v 3.11 -c conda-forge
 if [[ -z ${PREFIX+x} ]]; then
     ENV_NAME=system311-conda-forge
-    mamba env create -f temp.yml -n "$ENV_NAME"
+    if [[ -z "$UPDATE" ]]; then
+        mamba env create -f temp.yml -n "$ENV_NAME"
+    else
+        mamba env update -f temp.yml -n "$ENV_NAME" --prune
+    fi
     . activate "$ENV_NAME"
     PREFIX="$CONDA_PREFIX"
 else
-    mamba env create -f temp.yml -p "$PREFIX"
+    if [[ -z "$UPDATE" ]]; then
+        mamba env create -f temp.yml -p "$PREFIX"
+    else
+        mamba env update -f temp.yml -p "$PREFIX" --prune
+    fi
 fi
 rm -f temp.yml
 
