ecac694589e2d56aee0dbf16ab127835a65db254
Kolen Cheung <christian.kolen@gmail.com>
2023-04-25 19:00:11 +0000

    cleanup and update to 5.9

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
-VERSION=${VERSION:-5.5.1}
-DOWNLOADDIR=${DOWNLOADDIR:-"$HOME/.zsh"}
-INSTALLDIR=${INSTALLDIR:-/global/common/software/polar/local}
+set -e
+
+# TODO: update from https://zsh.sourceforge.io/Arc/source.html
+VERSION=${VERSION:-5.9}
+# https://unix.stackexchange.com/a/84980/192799
+DOWNLOADDIR="$(mktemp -d 2>/dev/null || mktemp -d -t 'zsh')"
+INSTALLDIR=${INSTALLDIR:-"$HOME/.local"}
 
 filename="zsh-$VERSION.tar.xz"
 dirname="zsh-$VERSION"
 url="https://sourceforge.net/projects/zsh/files/zsh/$VERSION/$filename/download"
 
-mdcd () {
-    mkdir -p "$@" && cd "$@"
+cd "$DOWNLOADDIR"
+
+# helpers ##############################################################
+
+print_double_line () {
+    eval printf %.0s= '{1..'"${COLUMNS:-$(tput cols)}"\}
 }
 
-mdcd "$DOWNLOADDIR"
+print_line () {
+    eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
 
+########################################################################
+
+print_double_line
+echo Downloading from $url to temp dir $DOWNLOADDIR
 wget -O - "$url" | tar -xJf -
 
 cd "$dirname"
 
-./Util/preconfig
-
+print_double_line
+echo Configuring...
 ./configure --prefix="$INSTALLDIR" \
             --datadir="$INSTALLDIR/share" \
             --enable-maildir-support \
@@ -31,10 +45,19 @@ cd "$dirname"
             --enable-cap \
             --enable-zsh-secure-free
 
+print_double_line
+echo Making...
+
 make
 
 make check
 
+print_double_line
+echo Installing...
 make install
 
 make install.info
+
+print_line
+echo Removing temp dir at $DOWNLOADDIR
+rm -rf "$DOWNLOADDIR"
