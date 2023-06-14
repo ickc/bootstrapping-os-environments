25844ffc3f25202cb452f7f5112e450bf661ebf0
Kolen Cheung <christian.kolen@gmail.com>
2023-06-14 14:28:09 -0700

    add btop.sh

---
+++ install/btop.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+PREFIX=${PREFIX:-~/.local}
+# https://unix.stackexchange.com/a/84980/192799
+DOWNLOADDIR="$(mktemp -d 2>/dev/null || mktemp -d -t 'zsh')"
+
+# helpers ##############################################################
+
+print_double_line () {
+    eval printf %.0s= '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
+
+print_line () {
+    eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
+
+########################################################################
+
+downloadUrl=https://github.com/aristocratos/btop/releases/latest/download/btop-x86_64-linux-musl.tbz
+filename="${downloadUrl##*/}"
+
+print_double_line
+echo Downloading to temp dir "$DOWNLOADDIR"
+cd "$DOWNLOADDIR"
+wget "$downloadUrl"
+tar -xf "$filename"
+
+print_double_line
+echo Installing...
+cd btop
+export PREFIX
+make install
+
+print_line
+echo Removing temp dir "$DOWNLOADDIR"
+rm -rf "$DOWNLOADDIR"
