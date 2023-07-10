224afa140546c034638f3c931161b63322023240
Kolen Cheung <christian.kolen@gmail.com>
2023-07-10 19:24:49 +0100

    remove read and replace with gh

--- unix-minimal/install.sh
+++ unix-minimal/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# usuage: BSOS_EMAIL=... ./install.sh
+
 set -e
 
 # helpers ##############################################################
@@ -17,6 +19,10 @@ print_line () {
 # make sure packages from bootstrap.sh can be seen
 export PATH="$HOME/.local/bin:$PATH"
 
+if [[ -z "$BSOS_EMAIL" ]]; then
+    echo "Error: BSOS_EMAIL environment variable not set. Exiting..."
+    exit 1
+fi
 # determine ssh algorithm to use
 if ssh -Q key | grep -q "ssh-ed25519"; then
     SSH_ALGO=ed25519
@@ -31,18 +37,16 @@ print_double_line
 if [[ -f ~/.ssh/id_${SSH_ALGO}.pubs ]]; then
     echo "SSH key already exists, assuming ssh-agent is setup to pull from GitHub and skip generating ssh key."
 else
-    read -p "Enter your email: " email
-    echo "Generating ssh key for $email"
+    echo "Generating ssh key for $BSOS_EMAIL"
     mkdir -p ~/.ssh
-    ssh-keygen -t "$SSH_ALGO" -C "$email" -f "$HOME/.ssh/id_${SSH_ALGO}"
+    ssh-keygen -t "$SSH_ALGO" -C "$BSOS_EMAIL" -f "$HOME/.ssh/id_${SSH_ALGO}"
     eval "$(ssh-agent -s)"
     ssh-add "$HOME/.ssh/id_${SSH_ALGO}"
-
-    print_line
-    echo "Add the following to your github account in https://github.com/settings/keys"
-    cat "$HOME/.ssh/id_${SSH_ALGO}.pub"
 fi
-read -p "Press enter to continue"
+
+# authenticate with GitHub
+~/git/source/bootstrapping-os-environments/install/gh.sh
+gh auth login --git-protocol ssh --web
 
 print_double_line
 echo "Installing bootstrapping-os-envrioments..."
