6290cfadf068f1e2044ca9e4b1b46a762414eda9
Kolen Cheung <christian.kolen@gmail.com>
2023-07-13 19:50:26 +0100

    use $HOME instead of ~

--- unix-minimal/bootstrap.sh
+++ unix-minimal/bootstrap.sh
@@ -21,11 +21,11 @@ else
     NOGIT=1
 fi
 
-mkdir -p ~/git/source
-cd ~/git/source
+mkdir -p "$HOME/git/source"
+cd "$HOME/git/source"
 if [[ $NOGIT -eq 0 ]]; then
-    mkdir -p ~/.ssh
-    ssh-keyscan github.com >> ~/.ssh/known_hosts
+    mkdir -p "$HOME/.ssh"
+    ssh-keyscan github.com >> "$HOME/.ssh/known_hosts"
     print_double_line
     echo "Cloning bootstrapping-os-environments..."
     git clone git@github.com:ickc/bootstrapping-os-environments.git || git clone https://github.com/ickc/bootstrapping-os-environments.git
@@ -40,18 +40,18 @@ fi
 
 print_double_line
 echo "Installing mamba..."
-CONDA_PREFIX=~/.mambaforge ~/git/source/bootstrapping-os-environments/install/mamba.sh
-# shellcheck disable=SC1090
-. ~/.mambaforge/bin/activate
+CONDA_PREFIX="$HOME/.mambaforge" "$HOME/git/source/bootstrapping-os-environments/install/mamba.sh"
+# shellcheck disable=SC1091
+. "$HOME/.mambaforge/bin/activate"
 
 print_line
 echo "Installing system packages from conda..."
-~/git/source/bootstrapping-os-environments/common/conda/conda-system.sh
+"$HOME/git/source/bootstrapping-os-environments/common/conda/conda-system.sh"
 export PATH="$HOME/.local/bin:$PATH"
 
 if [[ $NOGIT -eq 1 ]]; then
-    rm -rf ~/git/source/bootstrapping-os-environments
-    cd ~/git/source
+    rm -rf "$HOME/git/source/bootstrapping-os-environments"
+    cd "$HOME/git/source"
     git clone git@github.com:ickc/bootstrapping-os-environments.git || git clone https://github.com/ickc/bootstrapping-os-environments.git
 fi
 }
