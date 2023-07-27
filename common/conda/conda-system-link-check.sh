4118ca078cf0a6425d803bd54edeb604028cbad3
Kolen Cheung <christian.kolen@gmail.com>
2023-07-27 18:23:22 +0100

    add conda-system-link-check.sh

---
+++ common/conda/conda-system-link-check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while IFS= read -r command; do
+    # Check if the command is available in the PATH
+    command -v "$command" >/dev/null 2>&1 || echo "$command"
+done <conda-system-link.txt
