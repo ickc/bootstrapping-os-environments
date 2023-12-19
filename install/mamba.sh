342a6d75f7ea93bce88a32dcb8a1474e5ff4561c
Kolen Cheung <christian.kolen@gmail.com>
2023-12-19 16:19:26 +0000

    change to miniforge3
    now that mambaforge is deprecated

--- install/mamba.sh
+++ install/mamba.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-CONDA_PREFIX="${CONDA_PREFIX:-"$HOME/.mambaforge"}"
+CONDA_PREFIX="${CONDA_PREFIX:-"$HOME/.miniforge3"}"
 # https://unix.stackexchange.com/a/84980/192799
-DOWNLOADDIR="$(mktemp -d 2>/dev/null || mktemp -d -t 'mambaforge')"
+DOWNLOADDIR="$(mktemp -d 2>/dev/null || mktemp -d -t 'miniforge3')"
 
 # helpers ##############################################################
 
@@ -20,17 +20,17 @@ print_line () {
 
 install () {
 # https://github.com/conda-forge/miniforge
-downloadUrl="https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+downloadUrl="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 
 print_double_line
 echo Downloading to temp dir "$DOWNLOADDIR"
 cd "$DOWNLOADDIR"
-curl -L "$downloadUrl" -o Mambaforge.sh
+curl -L "$downloadUrl" -o Miniforge3.sh
 
 print_double_line
 echo Installing mamba...
-chmod +x Mambaforge.sh
-./Mambaforge.sh -b -s -p "$CONDA_PREFIX"
+chmod +x Miniforge3.sh
+./Miniforge3.sh -b -s -p "$CONDA_PREFIX"
 
 print_line
 echo Removing temp dir "$DOWNLOADDIR"
