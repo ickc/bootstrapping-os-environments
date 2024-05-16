9736fa04aa918c165639187de12aa0a83865bd7f
Kolen Cheung <christian.kolen@gmail.com>
2024-05-16 17:22:04 +0100

    joshuto.sh: update asset naming convention

--- install/joshuto.sh
+++ install/joshuto.sh
@@ -10,7 +10,7 @@ LATEST_RELEASE_URL="$(curl -s "$REPO_URL" | grep -oP '/kamiyaa/joshuto/releases/
 # e.g. v0.9.6
 LATEST_VERSION="$(echo "$LATEST_RELEASE_URL" | grep -oP '[^/]+$')"
 
-LATEST_DOWNLOAD_URL="https://github.com/kamiyaa/joshuto/releases/download/${LATEST_VERSION}/joshuto-${LATEST_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+LATEST_DOWNLOAD_URL="https://github.com/kamiyaa/joshuto/releases/download/${LATEST_VERSION}/joshuto-${LATEST_VERSION}-x86_64-unknown-linux-musl.tar.gz"
 
 echo "Downloading $LATEST_DOWNLOAD_URL"
 wget -qO- "$LATEST_DOWNLOAD_URL" | tar xz -C "$PREFIX/bin" --strip-components=1
