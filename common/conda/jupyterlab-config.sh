779c985817979054efe57358fc2a0e0a3c0db39d
Kolen Cheung <christian.kolen@gmail.com>
2024-06-05 17:03:19 +0100

    use JUPYTER_CONFIG_DIR

--- common/conda/jupyterlab-config.sh
+++ common/conda/jupyterlab-config.sh
@@ -2,25 +2,26 @@
 
 . activate jupyterlab
 
-mkdir -p "${HOME}/.jupyter"
+JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR}:-${HOME}/.jupyter}"
+mkdir -p "${JUPYTER_CONFIG_DIR}"
 
 if [[ $(uname) == Darwin ]]; then
-    cat << 'EOF' > "${HOME}/.jupyter/jupyter_lab_config.py"
+    cat << 'EOF' > "${JUPYTER_CONFIG_DIR}/jupyter_lab_config.py"
 c.ServerApp.browser = "/Applications/Chromium.app/Contents/MacOS/Chromium --app=%s"
 c.ServerApp.iopub_data_rate_limit = 10000000000
 EOF
 else
-    cat << 'EOF' > "${HOME}/.jupyter/jupyter_lab_config.py"
+    cat << 'EOF' > "${JUPYTER_CONFIG_DIR}/jupyter_lab_config.py"
 c.ServerApp.browser = "chromium-browser --app=%s"
 c.ServerApp.iopub_data_rate_limit = 10000000000
 EOF
 fi
 
 # https://jupyterlab.readthedocs.io/en/stable/user/jupyterhub.html#jupyterhub
-cat << EOF > "${HOME}/.jupyter/jupyterhub_config.py"
+cat << EOF > "${JUPYTER_CONFIG_DIR}/jupyterhub_config.py"
 c.Spawner.default_url = "/lab"
-c.JupyterHub.ssl_key = "${HOME}/.jupyter/ssl.key"
-c.JupyterHub.ssl_cert = "${HOME}/.jupyter/ssl.crt"
+c.JupyterHub.ssl_key = "${JUPYTER_CONFIG_DIR}/ssl.key"
+c.JupyterHub.ssl_cert = "${JUPYTER_CONFIG_DIR}/ssl.crt"
 EOF
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${HOME}/.jupyter/ssl.key" -out "${HOME}/.jupyter/ssl.crt"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${JUPYTER_CONFIG_DIR}/ssl.key" -out "${HOME}/.jupyter/ssl.crt"
