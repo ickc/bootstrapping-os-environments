79e492debd7858e8eb97f18a08734c783ac593d4
Kolen Cheung <christian.kolen@gmail.com>
2024-06-06 19:19:48 -0700

    fix FUSE not available

--- install/nvtop.sh
+++ install/nvtop.sh
@@ -3,11 +3,37 @@
 set -e
 
 PREFIX="${PREFIX:-${HOME}/.local}"
+IMAGEDIR="${IMAGEDIR:-${HOME}/.local/share/applications}"
 
 repo=Syllo/nvtop
 filename=nvtop-x86_64.AppImage
 downloadUrl="https://github.com/${repo}/releases/latest/download/${filename}"
 
+# helpers ##############################################################
+
+print_double_line() {
+    eval printf %.0s= '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
+
+print_line() {
+    eval printf %.0s- '{1..'"${COLUMNS:-$(tput cols)}"\}
+}
+
+########################################################################
+
+print_double_line
+echo "Downloading nvtop from ${downloadUrl}"
+mkdir -p "${IMAGEDIR}/nvtop"
+cd "${IMAGEDIR}/nvtop"
+wget "${downloadUrl}"
+
+print_double_line
+echo "Extracting nvtop to ${IMAGEDIR}/nvtop"
+chmod +x "${filename}"
+"./${filename}" --appimage-extract
+rm -f "${filename}"
+
+print_double_line
+echo "Creating a symbolic link to ${IMAGEDIR}/nvtop/squashfs-root/usr/bin/nvtop"
 mkdir -p "${PREFIX}/bin"
-echo "wget ${downloadUrl} -O ${PREFIX}/bin/nvtop"
-wget "${downloadUrl}" -O "${PREFIX}/bin/nvtop"
+ln -sf "${IMAGEDIR}/nvtop/squashfs-root/usr/bin/nvtop" "${PREFIX}/bin/nvtop"
