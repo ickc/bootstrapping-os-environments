4090eebbc410143d5b20105f3111039bbbbbc290
Kolen Cheung <christian.kolen@gmail.com>
2024-11-29 14:44:03 +0000

    Create nix.sh

---
+++ nix/nix.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# * This is a note, should not be executed as is
+
+# Determinate Systems Nix installer
+# https://zero-to-nix.com/start/install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
