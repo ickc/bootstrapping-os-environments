91c7c5ee80b7c6b4156d27358a02fcef19ce8f9b
Kolen Cheung <christian.kolen@gmail.com>
2024-11-29 14:42:39 +0000

    remove port, mas, jdk, mamba

--- macOS/install.sh
+++ macOS/install.sh
@@ -14,8 +14,6 @@ print_line() {
 
 ########################################################################
 
-./port-install.sh
-
 print_double_line
 echo "install homebrew..."
 if [[ "$(uname -m)" == arm64 ]]; then
@@ -27,18 +25,6 @@ else
     sudo mkdir -p "${HOMEBREW_PREFIX}" && sudo chown "${USER}" "${HOMEBREW_PREFIX}" && sudo chgrp staff "${HOMEBREW_PREFIX}"
     curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "${HOMEBREW_PREFIX}"
 fi
-export PATH="${HOMEBREW_PREFIX}/bin:${PATH}"
-print_double_line
-echo "install mas..."
-brew install mas
-
-print_double_line
-echo 'install oracle-jdk...'
-brew install --cask oracle-jdk
-
-print_double_line
-echo 'install mamba-forge...'
-../install/mamba.sh
 
 print_double_line
 echo 'install basher...'
