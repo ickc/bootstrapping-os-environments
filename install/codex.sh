3af08d8b9a4cf64ca92b00403eed403f3fb18c9f
Kolen Cheung <christian.kolen@gmail.com>
2026-05-08 11:14:08 +0100

    make all

--- install/codex.sh
+++ install/codex.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-__OPT_ROOT="${__OPT_ROOT:-"${HOME}/.local"}"
-MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-"${HOME}/.miniforge3"}"
+__OPT_ROOT="${__OPT_ROOT:-"${HOME}/.local/opt/${__OSTYPE}-${__ARCH}"}"
+MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-"${__OPT_ROOT}/miniforge3"}"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
 ZDOTDIR="${ZDOTDIR:-"${HOME}"}"
 BINDIR="${__OPT_ROOT}/bin"
